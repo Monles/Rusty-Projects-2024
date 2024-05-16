4cfc3cb2713c627179e5c1a9f5bd6c01bfa7b125
MonLes <73730090+Monles@users.noreply.github.com>
2024-05-16 12:54:17 +0100

    update

--- snake_game/src/constants.rs
+++
@@ -1,6 +0,0 @@
-pub const ARENA_WIDTH: u32 = 10;
-pub const ARENA_HEIGHT: u32 = 10;
-pub const SNAKE_HEAD_COLOR: (f32, f32, f32) = (0.7, 0.7, 0.7);
-pub const SNAKE_SEGMENT_COLOR: (f32, f32, f32) = (0.3, 0.3, 0.3);
-pub const FOOD_COLOR: (f32, f32, f32) = (1.0, 0.0, 0.0);
-pub const GRID_SIZE: f32 = 64.0;
